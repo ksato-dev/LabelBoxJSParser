529701a32784bf41ef436edb59326a588d2449dd
ksato-dev <k-sato@smartrobotics.jp>
2020-04-23 17:18:46 +0900

    Can refer to dim1-polyfit data in all available images.

--- test_read.py
+++ test_read.py
@@ -12,7 +12,8 @@ print("project name:", prj_name)
 print()
 
 # fy_all = list(range(num_pts - 1))
-fy_all = list(range(num_json_data))
+# fy_all = list(range(num_json_data))
+fy_all = []
 
 for index, curr_json in enumerate(json_load):
     if ("objects" in curr_json["Label"]) == False:
@@ -25,28 +26,29 @@ for index, curr_json in enumerate(json_load):
     temp_fname1 = labeled_data.rsplit(".png?")[0]
     temp_fname2 = temp_fname1.rsplit("-")[-1]
     src_fname = temp_fname2 + ".png"
-    print("source file name:", src_fname)
+    # print("source file name:", src_fname)
 
     ## parse annotational lines in current image. ##
     objects = curr_json["Label"]["objects"]
     num_objs = len(objects)
-    print("num objects:", num_objs)
+    # print("num objects:", num_objs)
 
     fy_objs = list(range(num_objs))
-    fy_all[index] = (fy_objs)
+    # fy_all[index] = (fy_objs)
 
     for obj_id, curr_obj in enumerate(objects):
         curr_lines = curr_obj["line"]
-        print("line no.", obj_id)
+        # print("line no.", obj_id)
 
         num_pts = len(curr_lines)
+
         fy = list(range(num_pts - 1))
         before_point = None
         for point_id, curr_point in enumerate(curr_lines):
             x = curr_point["x"]
             y = curr_point["y"]
-            msg = "pt[{0:d}]:(x, y) = ({1:f}, {2:f})".format(point_id, x, y)
-            print(msg)
+            # msg = "pt[{0:d}]:(x, y) = ({1:f}, {2:f})".format(point_id, x, y)
+            # print(msg)
 
             if (before_point != None):
                 ## create x = f(y)
@@ -61,13 +63,30 @@ for index, curr_json in enumerate(json_load):
                 # print(curr_fy(0.0))
                 
                 ## [x = f(y)], [y_i-1, y_i]
-                fy[point_id - 1] = (curr_fy, (bf_y, y))
+                fy[point_id - 1] = (curr_fy, (float(bf_y), float(y)))
 
             before_point = curr_point
 
-        fy_all[index][obj_id] = fy
+        # fy_all[index][obj_id] = fy
+        fy_objs[obj_id] = fy
         # print(fy_all[index][obj_id][0])
+    fy_all.append((src_fname, fy_objs))
 
+num_imgs = len(fy_all)
+print(num_imgs)
+for fname, fy_objs in fy_all:
+    # for pl_id, fy_polyline in enumerate(fy_objs):
+    # fy_objs = fy_all[im_id]
+    # print(type(fy_objs))
+    print(fname)
+    print(fy_objs)
+    num_objs = len(fy_objs)
+    # for ply_id in range(num_objs):
+    #     for f_id, fy in enumerate(fy_polyline):
+    #         msg = "[{0:d}][{1:d}][{2:d}]".format(im_id, -1, f_id)
+    #         # print(msg)
+    #          #print(fy)
     print()
+
 
 # print(json_load)
