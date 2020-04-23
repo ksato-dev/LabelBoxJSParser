78b7e8cd951945aab1fe1f216f9743e19f42dfb8
ksato-dev <k-sato@smartrobotics.jp>
2020-04-23 16:28:00 +0900

    Can get polyfit lines in all images.

--- test_read.py
+++ test_read.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 # json_open = open("labelbox_data.json", "r")
 json_open = open("clips_001.json", "r")
 json_load = json.load(json_open)
@@ -9,6 +10,9 @@ print("num data:", num_json_data)
 prj_name = json_load[0]["Project Name"]
 print("project name:", prj_name)
 print()
+
+# fy_all = list(range(num_pts - 1))
+fy_all = list(range(num_json_data))
 
 for index, curr_json in enumerate(json_load):
     if ("objects" in curr_json["Label"]) == False:
@@ -23,12 +27,46 @@ for index, curr_json in enumerate(json_load):
     src_fname = temp_fname2 + ".png"
     print("source file name:", src_fname)
 
+    ## parse annotational lines in current image. ##
     objects = curr_json["Label"]["objects"]
-
     num_objs = len(objects)
     print("num objects:", num_objs)
-    for curr_obj in objects:
-        print(curr_obj["line"])
+
+    fy_objs = list(range(num_objs))
+    fy_all[index] = (fy_objs)
+
+    for obj_id, curr_obj in enumerate(objects):
+        curr_lines = curr_obj["line"]
+        print("line no.", obj_id)
+
+        num_pts = len(curr_lines)
+        fy = list(range(num_pts - 1))
+        before_point = None
+        for point_id, curr_point in enumerate(curr_lines):
+            x = curr_point["x"]
+            y = curr_point["y"]
+            msg = "pt[{0:d}]:(x, y) = ({1:f}, {2:f})".format(point_id, x, y)
+            print(msg)
+
+            if (before_point != None):
+                ## create x = f(y)
+                bf_x = before_point["x"]
+                bf_y = before_point["y"]
+
+                pt_x = np.array([bf_x, x])
+                pt_y = np.array([bf_y, y])
+
+                k = np.polyfit(pt_y, pt_x, 1)
+                curr_fy = np.poly1d(k)
+                # print(curr_fy(0.0))
+                
+                ## [x = f(y)], [y_i-1, y_i]
+                fy[point_id - 1] = (curr_fy, (bf_y, y))
+
+            before_point = curr_point
+
+        fy_all[index][obj_id] = fy
+        # print(fy_all[index][obj_id][0])
 
     print()
 
